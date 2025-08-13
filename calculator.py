f0f5f731daf8a8203b9002a749ccf578d1e40aab
vortexedits <rishabashmik@gmail.com>
2025-08-13 11:06:37 +0530

    working problemm

--- calculator.py
+++ calculator.py
@@ -2,3 +2,6 @@
 a = 10
 b = 20
 print("addition of two number is;", a+b)
+print("substraction of two number is;", a-b)
+print("multiplication of two number is;", a*b)
+print("division of two number is;", a/b)
